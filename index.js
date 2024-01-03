856ce33784d86b038db40bb116956b8c4fcc9738
Dragonsfure <ae_a18@outlook.de>
2024-01-03 16:32:57 +0100

    Fixxed Issue

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
