f19be1373fcc9efbdda2b0c85cce6d4964785b6a
Dragonsfure <ae_a18@outlook.de>
2024-01-08 20:13:25 +0100

    Added something

--- src/commands/host.ts
+++ src/commands/host.ts
@@ -1,52 +1,70 @@
-import { CommandInteraction, SlashCommandBuilder, EmbedBuilder, CommandInteractionOptionResolver, TextChannel, Guild } from "discord.js";
+import {
+  CommandInteraction,
+  SlashCommandBuilder,
+  EmbedBuilder,
+  CommandInteractionOptionResolver,
+  TextChannel,
+  Guild,
+} from "discord.js";
 import { client } from "..";
+import ms from "ms";
 
 export const data = new SlashCommandBuilder()
 
   .setName("host")
   .setDescription("Start Hosting the Hunger Games")
-  .addChannelOption((option) => 
+  .addChannelOption((option) =>
     option
-    .setName("channel")
-    .setDescription("What channel would you like this message to appear in")
-    .setRequired(true)
+      .setName("channel")
+      .setDescription("What channel would you like this message to appear in")
+      .setRequired(true)
   )
   .addStringOption((op) =>
     op
-    .setName("message")
-    .setDescription("What message would you like to appear in the channel")
-    .setRequired(true)
+      .setName("message")
+      .setDescription("What message would you like to appear in the channel")
+      .setRequired(true)
   )
-  .addIntegerOption((timer) =>
-  timer 
-  .setName("time")
-  .setDescription("How much time before the hunger games begins")
-  .setRequired(true)
+  .addStringOption((timer) =>
+    timer
+      .setName("time")
+      .setDescription("How much time before the hunger games begins")
+      .setRequired(true)
   );
 
 export async function execute(interaction: CommandInteraction) {
   const info = interaction.options as CommandInteractionOptionResolver;
   const channel = info.getChannel("channel");
   const message = info.getString("message");
-  const timer = info.getInteger("time");
+  const timer = info.getString("time");
   const guildId = interaction.guildId;
-  if(guildId){
-    const guild: Guild = client.guilds.cache.get(guildId) as Guild;             
-    const channell: TextChannel =     guild.channels.cache.get(channel!.id) as TextChannel
+
+  const timerrr = ms(timer as string);
+
+  if (guildId) {
+    const guild: Guild = client.guilds.cache.get(guildId) as Guild;
+    const channell: TextChannel = guild.channels.cache.get(
+      channel!.id
+    ) as TextChannel;
     const exampleEmbed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle("Hunger games")
-	.setDescription(message)
-	.setTimestamp()
-	.setFooter({ text: "Hosted by Hunger games bot", iconURL: `${client.user?.avatarURL()}` })
-    ;
-    if(message){
+
+      .setColor(0x0099ff)
+      .setTitle("Hunger games")
+      .setDescription(message)
+      .setTimestamp()
+      .setFooter({
+        text: "Hosted by Hunger games bot",
+        iconURL: `${client.user?.avatarURL()}`,
+      });
+    if (message) {
       channell.send({ embeds: [exampleEmbed] }).then((embedMessage) => {
         embedMessage.react("👍");
       });
-      }
-  }             
-  return interaction.reply("Done! check your message in " + `<#${channel?.id}>`);
+    }
+  }
+  const extraInfo = isNaN(timerrr) ? "" : `The timer is ${timerrr} in ms`;
+  
+  return interaction.reply(
+    "Done! check your message in " + `<#${channel?.id}>   ${extraInfo}`
+  );
 }
-
-
