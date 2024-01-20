22e1cddb15f952f82c3aaeb2f44f7fb10e71956b
Dragonsfure <ae_a18@outlook.de>
2024-01-20 21:33:02 +0100

    Html factory, Test Image, Styles and test Html

--- src/HtmlFactory.ts
+++ src/HtmlFactory.ts
@@ -1,6 +1,9 @@
 import { District } from "./types/District";
+import { Game } from "./types/Game";
+import fs from "fs";
+import { FindCorrespondingValue } from "./helpers/intervalMap";
 
-export function CreateHtmlDistrict(district: District):string {
+export function CreateHtmlDistrict(district: District): string {
   let str = "";
 
   for (let i = 0; i < district.Players.length; i++) {
@@ -19,4 +22,36 @@ export function CreateHtmlDistrict(district: District):string {
     </div>`;
 
   return result;
+}
+
+const styles = fs.readFileSync("view\\styles.txt");
+const template = `<html><head> ${styles} </head> <body>  {0} </body> </html>`;
+
+export function CreateHtml(gameInstance: Game): string[] {
+  //
+  const htmlStrings: string[] = [];
+
+  if (
+    gameInstance.Districts.length > 0 &&
+    gameInstance.Districts[0].Players.length > 0
+  ) {
+    const amountOfPlayer = gameInstance.Districts[0].Players.length;
+    const maxDistrict = FindCorrespondingValue(amountOfPlayer);
+
+    let districtHelper = "";
+    let x = 0;
+    for (let i = 0; i < gameInstance.Districts.length; i++) {
+      districtHelper += CreateHtmlDistrict(gameInstance.Districts[i]);
+      x++;
+      if (x >= maxDistrict || i >=gameInstance.Districts.length) {
+        const str = template.replace("{0}", districtHelper);
+        htmlStrings.push(str);
+        x = 0;
+        districtHelper = ""; 
+      }
+      
+    }
+  }
+
+  return htmlStrings;
 }
