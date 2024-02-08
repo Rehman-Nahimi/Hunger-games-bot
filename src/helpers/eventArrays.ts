6cea308840b54a3eedcdf3cf040bcb12dcac40af
Rehman Nahimi <rehman2000@live.com>
2024-02-08 13:52:08 +0000

    new scenarios

--- src/helpers/eventArrays.ts
+++ src/helpers/eventArrays.ts
@@ -41,3 +41,30 @@ export const deathScenario = new DeathScenario([
   "{0} accidentally cuts himself and gets infected. ",
   "{0} bleeds out due to untreated wounds",
 ]);
+
+export const injuryScenario = new DeathScenario([
+  "{0} loses leg to a man eating monkey.",
+  "{0} falls over and gets a concussion",
+  "{0} touches a poisonous flower",
+  "{0} is bit by a rare spider species ",
+  "{0} catches a very bad illness",
+]);
+
+export const lightInjuryScenario = new DeathScenario([
+  "{0} sprains ankle while tripping up thinking about their crush",
+  "{0} pulls a leg while running ",
+  "{0} injures his shoulder lifting heavy objects",
+]);
+
+export const lightBuffScenario = new DeathScenario([
+  "{0} finds old tools in a cave",
+  "{0} is given water and bandages from a sponsor",
+  "{0} finds rabbits to hunt and eat",
+  "{0} wakes up from a nap about his crush, they feel energetic",
+]);
+
+export const buffScenario = new DeathScenario([
+  "{0} finds a gun with 3 bullets",
+  "{0} is given a medkit from a sponsor ",
+  "{0} steals another parties food and supplies",
+]);
